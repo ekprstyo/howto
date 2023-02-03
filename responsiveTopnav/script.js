d7398cc4a5f43b11c5a7365e4ad972b30096464f
pra-setiyo <prassonly@gmail.com>
2023-02-03 16:30:07 +0700

    add responsive top nav

---
+++ responsiveTopnav/script.js
@@ -0,0 +1,8 @@
+function responsive() {
+  const topnav = document.getElementById("mytopnav");
+  if (topnav.className === "topnav") {
+    topnav.className += " responsive";
+  } else {
+    topnav.className = "topnav";
+  }
+}
